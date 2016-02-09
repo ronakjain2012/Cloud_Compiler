7afccc49d47e60037463f42504fbd84d7e2c4761
ronakjain2012 <ronakjain2012@gmail.com>
2016-02-10 01:51:36 +0530

    CC

---
+++ index.php
@@ -0,0 +1,96 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Cloud Compiler</title>
+
+<!-- Stylesheet files-->
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body class="main-body">
+<div class="col-lg-12 col-md-12 col-sm-12">
+  <div>
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header"> <a class="navbar-brand" style="color:#EDEDED;"> &#60; &#47; &#62; Cloud Complier</a> </div>
+        <ul class="nav navbar-nav">	
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Sign Out</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Sign In</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Sign Up</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="search col-lg-12 col-md-11 col-sm-11">
+      <form method="post" action="#">
+        <div class="col-lg-1 col-md-1 col-sm-1"> &nbsp; </div>
+        <div class="input-group col-lg-10 col-md-9 col-sm-9"> <span class="input-group-addon small-border-search-right" id="basic-addon1"> <span class="glyphicon glyphicon-search"></span> </span>
+          <input type="search" class="form-control small-border" placeholder="Search" name="CCSearch" aria-describedby="basic-addon1" />
+        </div>
+        <div class="col-lg-12 col-md-11 col-sm-11"> <br/>
+          <input type="submit" class="btn btn-success form-control" value="Search" />
+        </div>
+      </form>
+    </div>
+    <div class="languages">
+      <div class="row">
+        <div class="col-md-3 col-sm-3">
+          <div class="thumbnail"> <img src="images/PHP.png" alt="PHP">
+            <div class="caption">
+              <p> <!-- Any Text --></p>
+              <p><a href="#" class="btn btn-primary" role="button">Go</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="thumbnail"> <img src="images/PHPsql.png" alt="PHP">
+            <div class="caption">
+              <p> <!-- Any Text --></p>
+              <p><a href="#" class="btn btn-primary" role="button">Go</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="thumbnail"> <img src="images/sql.png" alt="PHP">
+            <div class="caption">
+              <p> <!-- Any Text --></p>
+              <p><a href="#" class="btn btn-primary" role="button">Go</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3">
+          <div class="thumbnail"> <img src="images/pl-sql.png" alt="PHP">
+            <div class="caption">
+              <p> <!-- Any Text --></p>
+              <p><a href="#" class="btn btn-primary" role="button">Go</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+      <div class="col-md-3 col-sm-3">
+          <div class="thumbnail"> <img src="images/Batch.png" alt="PHP">
+            <div class="caption">
+              <p> <!-- Any Text --></p>
+              <p><a href="#" class="btn btn-primary" role="button">Go</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+<!-- JavaScript Files -->
+<script src="js/bootstrap.js"></script>
+</html>
